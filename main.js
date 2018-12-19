86fc97837b3f5a92385c06c95042d715f2c0913c
André <andre.iske@mailbox.org>
2018-12-19 19:02:19 +0100

    Added some test debug log

--- main.js
+++ main.js
@@ -96,9 +96,7 @@ function main() {
 
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
-    adapter.log.info('config test1: '    + adapter.config.test1);
-    adapter.log.info('config test1: '    + adapter.config.test2);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+    adapter.log.info('config host: '    + adapter.config.host);
 
 
     /**
