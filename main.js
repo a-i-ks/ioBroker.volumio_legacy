4d13213158da2d6bd61b6e80e4773c3ca4b21ebf
André <andre.iske@mailbox.org>
2018-12-29 00:45:27 +0100

    Further work on adapter logic

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-/**
- *
- * volumio adapter
- *
- *
- *  file io-package.json comments:
- *
- *  {
- *      "common": {
- *          "name":         "volumio",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js volumio Adapter",  // Adapter title shown in User Interfaces
- *          "authors":  [                               // Array of authord
- *              "name <mail@volumio.com>"
- *          ]
- *          "desc":         "volumio adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
- *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
- *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
- *          "materialize":  true,                       // support of admin3
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
- *          "loglevel":     "info"                      // Adapters Log Level
- *      },
- *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
- *          "test1": true,
- *          "test2": 42,
- *          "mySelect": "auto"
- *      }
- *  }
- *
- */
-
 /* jshint -W097 */// jshint strict:false
 /*jslint node: true */
 'use strict';
@@ -67,17 +36,47 @@ adapter.on('unload', function (callback) {
 adapter.on('objectChange', function (id, obj) {
     // Warning, obj can be null if it was deleted
     adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+
 });
 
 // is called if a subscribed state changes
+// id => volumio.0.player.pause
+// obj => {"val":true,"ack":false,"ts":1545603169862,"q":0,"from":"system.adapter.admin.0","lc":1545603169862}
 adapter.on('stateChange', function (id, state) {
     // Warning, state can be null if it was deleted
     adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
-
+    if (!id) return
+    // if (!state || state.ack) return; //TODO Check this
+    // frage klären obj != null => ist dann immer string vergleich möglich?
+    // TODO check this
     // you can use the ack flag to detect if it is status (true) or command (false)
-    if (state && !state.ack) {
-        adapter.log.info('ack is not set!');
+    //if (state && !state.ack) {
+    //    adapter.log.info('ack is not set!');
+    //} 
+    
+    switch (id) {
+        case "volumio.0.player":
+        case "volumio.0.player.pause":
+        // Todo if played on spotify connect send a message via spotify
+        // on/off in einstellungen + select spotify device  
+            sendCmdPause();
+            break;
+        case "volumio.0.player.toggle":
+            sendCmdToggle();
+            break;
+        case "volumio.0.player.play": 
+            sendCmdPlay()
+            break;
+        case "volumio.0.player.playN":
+            if (state && state.val) {
+                sendCmdPlay(state.val)
+            }
+            break; 
+        case "volumio.0.player.stop":
+            sendCmdStop();
+            break;
     }
+
 });
 
 // Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
@@ -105,7 +104,7 @@ function sendRequest(endpoint, method, sendBody) {
         method: method,
         form: sendBody
     };
-    adapter.log.debug('volumino api call...' + endpoint + '; ' + options.form);
+    adapter.log.debug('volumio api call...' + endpoint + '; ' + options.form);
     let callStack = new Error().stack;
     return request(options)
         .then(function(response) {
@@ -124,6 +123,8 @@ function sendRequest(endpoint, method, sendBody) {
             switch (response.statusCode) {
                 case 200:
                     // OK
+                    adapter.log.debug('200 OK');
+                    adapter.log.debug('http response: ' + JSON.stringify(response));
                     ret = parsedBody;
                     break;
                 case 202:
@@ -188,66 +189,38 @@ function sendRequest(endpoint, method, sendBody) {
 }
 
 function main() {
+    setup();
+}
 
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-    adapter.log.info('config host: '    + adapter.config.host);
-    adapter.log.info('sending test api request ...')
-    sendRequest('/api/v1/getstate', 'GET', '')
-
-
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple volumio for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
-
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            name: 'testVariable',
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
-    });
-
-    // in this volumio all states changes inside the adapters namespace are subscribed
+function setup() {
     adapter.subscribeStates('*');
+}
 
 
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
+function sendCmdPause() {
+    sendRequest('/api/v1/commands/?cmd=pause', 'GET', '');
+}
 
-    // the variable testVariable is set to true as command (ack=false)
-    adapter.setState('testVariable', true);
+function sendCmdToggle() {
+    sendRequest('/api/v1/commands/?cmd=pause', 'GET', '');
+}
 
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    adapter.setState('testVariable', {val: true, ack: true});
+function sendCmdPlay(n) {
+    if(n && Number.isInteger(n) ) {
+        sendRequest('/api/v1/commands/?cmd=play&N='+n, 'GET', '');
+    } else {
+        sendRequest('/api/v1/commands/?cmd=play', 'GET', '');
+    }
+}
 
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    adapter.setState('testVariable', {val: true, ack: true, expire: 30});
+function sendCmdStop() {
+    sendRequest('/api/v1/commands/?cmd=stop', 'GET', '');
+}
 
+function sendCmdPrev() {
+    sendRequest('/api/v1/commands/?cmd=prev', 'GET', '');
+}
 
-
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
-
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
-
-
-
+function sendCmdNext() {
+    sendRequest('/api/v1/commands/?cmd=next', 'GET', '');
 }
